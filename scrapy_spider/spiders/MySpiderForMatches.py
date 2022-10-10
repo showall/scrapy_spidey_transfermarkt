d4f5f50041227b32aef01ad882e4adb4c390f1a6
showall <gerardsho@gmail.com>
2022-10-10 16:03:36 +0800

    first commit

---
+++ scrapy_spider/spiders/MySpiderForMatches.py
@@ -0,0 +1,221 @@
+import scrapy
+from scrapy.spiders import CrawlSpider,  Rule
+from scrapy.linkextractors import LinkExtractor
+import re
+
+class MySpiderForMatches(CrawlSpider):
+
+    user_agent ="Custom"
+    name = "match_logs"
+    allowed_domains = ["www.transfermarkt.com"]
+    custom_settings = {
+        "DOWNLOAD_DELAY" : 4,
+        "DOWNLOAD_TIMEOUT" : 20,
+        "CONCURRENT_REQUESTS" : 20,
+        'FEEDS': {
+            '%(name)s.csv': {
+                'format': 'csv',
+                'overwrite': True
+            }
+        }
+    }    
+    
+    def __init__(self, domain='', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if domain != '':
+            self.start_urls = [domain]
+        else:
+            self.start_urls =  ["https://www.transfermarkt.com/manchester-united/spielplan/verein/985/saison_id/2022"]
+
+
+
+    rules = (
+    #    Rule(LinkExtractor(allow = r'profil\/spieler'), callback='parse_players', follow=True)
+      #  Rule(LinkExtractor(allow = r'startseite\/verein.*saison_id\/\d{3}2'), callback='parse_club_links', follow=False),
+        Rule(LinkExtractor(allow = r'spielplan\/verein.*saison_id\/\d{3}2'), follow=False),     
+        Rule(LinkExtractor(allow = r'spielbericht\/index'), callback='parse_club_links_2', follow=False),    
+    # Rule(LinkExtractor(allow = r'aufstellung\/spielbericht'), callback='parse_match_sheet_sub', follow=False),   
+        #   Rule(LinkExtractor(allow = r'profil\/spieler'), callback='parse_players', follow=True)
+    )
+
+    def parse_club_links_2(self,response):
+  
+        attributes = {}
+        attributes["match_id"] = response.url.split("/")[-1]
+        attributes["home_tag"] = response.xpath('//a[@class="sb-vereinslink"]/@href').getall()[0].strip().split("/")[1]          
+        attributes["away_tag"] = response.xpath('//a[@class="sb-vereinslink"]/@href').getall()[1].strip().split("/")[1]
+        matchsheet_link =  f'https://www.transfermarkt.com/{attributes["home_tag"]}_{attributes["away_tag"]}/index/spielbericht/{attributes["match_id"]}'
+        yield scrapy.Request(url=matchsheet_link, callback=self.parse_match_sheet, headers={'User-Agent': 'Custom'}, 
+        meta = {"attributes" : attributes}, dont_filter=True)
+
+    def parse_match_sheet(self,response):
+        
+        attributes = response.meta["attributes"]
+       # attributes["trfmarkt_game_id"] =  response.url.split("/")[-1]
+        attributes["home_team"] = response.xpath('//a[@class="sb-vereinslink"]/text()').getall()[0].strip() 
+        attributes["away_team"] = response.xpath('//a[@class="sb-vereinslink"]/text()').getall()[1].strip()     
+
+        try:
+            row = response.xpath('//p[@data-type="link"]/text()').getall()
+            attributes["home_team_position"] = row[0].replace("Position:","").strip()
+            attributes["away_team_position"] = row[1].replace("Position:","").strip()        
+        except:
+            attributes["home_team_position"] = "nil"
+            attributes["away_team_position"] = "nil"      
+
+        try:
+            row = response.xpath('//div[@class="sb-endstand"]/text()').get().split(":")
+            attributes["home_team_score"] =  row[0].strip()
+            attributes["away_team_score"] =  row[1].strip()    
+        except:
+            attributes["home_team_score"] = "nil"
+            attributes["away_team_score"]= "nil"     
+        
+        try:
+            attributes["season"] = re.search("(\d{4})",response.xpath('//div[@class="spielername-profil"]//a/@href').get()).group(0)
+            attributes["competition"] =  response.xpath('//div[@class="spielername-profil"]//a/text()').get()
+
+        except:
+            attributes["season"] = "nil"
+            attributes["competition"] = "nil"
+
+        try:
+            string = str([x.strip() for x in response.xpath('//p[contains(@class,"sb-datum hide-for-small")]//text()').getall()[0:2]])
+            attributes["event"] =  re.sub(r"(:)|(\])|(\[)|(\|)|(,)|(')|([A-Z][a-z]{2}, \d+\/\d+\/\d+)","",string).strip()
+        except:
+            attributes["event"] = "nil"
+        try:
+            attributes["date"] =  response.xpath('//p//a[contains(@href,"aktuell/waspassiertheute")]/text()').getall()[0].strip()
+ 
+        except:
+            attributes["date"] =  "nil"
+        try:
+            attributes["time"] =  re.search(r"\d+:\d+ .M",response.xpath('//p//a[contains(@href,"aktuell/waspassiertheute")]/../text()').getall()[2]).group(0)
+        except:
+            try:
+                attributes["time"] =  re.search(r"\d+:\d+ .M",response.xpath('//p//a[contains(@href,"aktuell/waspassiertheute")]/../text()').getall()[1]).group(0)
+            except:
+                attributes["time"] =  "nil"        
+        try:            
+            attributes["referee"] =  response.xpath('//p[@class="sb-zusatzinfos"]//a/text()').getall()[1].strip()                
+        except:
+            attributes["referee"] =  "nil"
+        try:
+            attributes["away_ht_team_score"] =  response.xpath('//div[@class="sb-halbzeit"]/span/text()').get().replace(":", "").strip()
+            attributes["home_ht_team_score"] =  response.xpath('//div[@class="sb-halbzeit"]/span/../text()').getall()[1].replace(")","")
+
+        except:
+            attributes["away_ht_team_score"] =   "nil"
+            attributes["home_ht_team_score"] =  "nil"
+
+        try :      
+            attributes["home_formation"] = response.xpath('//*[@class="large-7 aufstellung-vereinsseite columns small-12 unterueberschrift aufstellung-unterueberschrift"]/text()').getall()[0].replace("Starting Line-up:","").strip()
+            attributes["away_formation"] = response.xpath('//*[@class="large-7 aufstellung-vereinsseite columns small-12 unterueberschrift aufstellung-unterueberschrift"]/text()').getall()[1].replace("Starting Line-up:","").strip()
+        except:
+            attributes["home_formation"] =   "nil" 
+            attributes["away_formation"] =   "nil"        
+
+        try :
+            manager = response.xpath('//div[contains(text(),"Manager")]/parent::*/following-sibling::*/a/text()').getall()
+            attributes["home_manager"] = manager[0]
+            attributes["away_manager"] = manager[1]
+        except:    
+            attributes["home_manager"] = "nil"
+            attributes["away_manager"] = "nil"       
+
+        try :
+            attributes["attendance"] = response.xpath('//p[@class="sb-zusatzinfos"]//strong/text()').get().replace("Attendance:", "").strip().replace(".","")
+        except :
+            attributes["attendance"] = "nil"   
+
+        line_up = f'https://www.transfermarkt.com/{attributes["home_tag"]}_{attributes["away_tag"]}/aufstellung/spielbericht/{attributes["match_id"]}' 
+        yield scrapy.Request(url=line_up, callback=self.parse_lineup, headers={'User-Agent': 'Custom'}, meta= {"attributes" : attributes}, dont_filter=True)
+
+    def parse_lineup(self,response):
+
+        attributes =  response.meta["attributes"]
+        try:
+            attributes["home_team_foreigners%"] =re.search(r"\d+.\d+%*", response.xpath('//div[@class="table-footer"]//td[contains(text(),"Foreigners")]/text()').getall()[0]).group(0)
+            attributes["away_team_foreigners%"] = re.search(r"\d+.\d+%*", response.xpath('//div[@class="table-footer"]//td[contains(text(),"Foreigners")]/text()').getall()[1]).group(0)
+            attributes["home_team_avg_age"] = re.search(r"(\d{2}.\d+)", response.xpath('//div[@class="table-footer"]//td[contains(text(),"age")]/text()').getall()[0]).group(0)
+            attributes["away_team_avg_age"] = re.search(r"(\d{2}.\d+)", response.xpath('//div[@class="table-footer"]//td[contains(text(),"age")]/text()').getall()[1]).group(0)
+  
+        except:
+            pass
+
+        try:
+            attributes["home_team_PV"] =re.search(r"€(\d+.\d+)m", response.xpath('//div[@class="table-footer"]//td[contains(text(),"value")]/text()').getall()[0]).group(0)
+        except:
+            try:
+                attributes["home_team_PV"] =re.search(r"€(\d+.\d+)Th", response.xpath('//div[@class="table-footer"]//td[contains(text(),"value")]/text()').getall()[0]).group(0)
+            except:
+                    attributes["home_team_PV"] = ""
+
+        try:
+            attributes["away_team_PV"] =re.search(r"€(\d+.\d+)m", response.xpath('//div[@class="table-footer"]//td[contains(text(),"value")]/text()').getall()[1]).group(0)
+        except:
+            try:
+                attributes["away_team_PV"] =re.search(r"€(\d+.\d+)Th", response.xpath('//div[@class="table-footer"]//td[contains(text(),"value")]/text()').getall()[1]).group(0)
+            except:
+                    attributes["away_team_PV"] = ""
+                    
+        try:
+            attributes["home_team_MV"] =re.search(r"€(\d+.\d+)m", response.xpath('//div[@class="table-footer"]//td[contains(text(),"MV")]/text()').getall()[0]).group(0)
+        except:
+            try:
+                attributes["home_team_MV"] =re.search(r"€(\d+.\d+)Th", response.xpath('//div[@class="table-footer"]//td[contains(text(),"MV")]/text()').getall()[0]).group(0)
+            except:
+                    attributes["home_team_MV"] = ""
+                    
+        try:
+            attributes["away_team_MV"] =re.search(r"€(\d+.\d+)m", response.xpath('//div[@class="table-footer"]//td[contains(text(),"MV")]/text()').getall()[1]).group(0)
+        except:
+            try:
+                attributes["away_team_MV"] =re.search(r"€(\d+.\d+)Th", response.xpath('//div[@class="table-footer"]//td[contains(text(),"MV")]/text()').getall()[1]).group(0)
+            except:
+                    attributes["away_team_MV"] = ""
+
+
+        statistics = f'https://www.transfermarkt.com/{attributes["home_tag"]}_{attributes["away_tag"]}/statistik/spielbericht/{attributes["match_id"]}'
+       # statistics = f"https://www.transfermarkt.com/{home_tag}_{away_tag}/statistik/spielbericht/{match_id}"  
+        yield scrapy.Request(url=statistics, callback=self.parse_match_stats, headers={'User-Agent': 'Custom'}, meta= {"attributes" : attributes} , dont_filter=True) 
+        #yield attributes
+       # yield scrapy.Request(response.url, callback=self.parse_printed, headers={'User-Agent': 'Custom'}, meta= {"attributes" : attributes} , dont_filter=True)
+        print("ok2")        
+    
+    def parse_match_stats(self,response):
+        #### THE POSSESSION% STILL A PROBLEM
+        attributes = response.meta["attributes"]
+        attributes["available_capacity"] =  response.xpath('//th[contains(text(),"Available Capacity:")]/following-sibling::*/text()').get().replace(".","")
+    
+        try:   
+            attributes["home_total_shots"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[0]
+            attributes["away_total_shots"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[1]
+
+            attributes["home_shots_off_target"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[2]
+            attributes["away_shots_off_target"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[3]
+
+            attributes["home_shots_saved"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[4]
+            attributes["away_shots_saved"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[5]
+
+            attributes["home_corners"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[6]
+            attributes["away_corners"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[7]
+
+            attributes["home_freekicks"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[8]
+            attributes["away_freekicks"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[9]
+
+            attributes["home_fouls"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[10]
+            attributes["away_fouls"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[11]
+
+            attributes["home_offsides"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[12]
+            attributes["away_offsides"] = response.xpath('//div[@class="sb-statistik-zahl"]/text()').getall()[13]            
+            
+        except:
+            pass
+        print("Parsing........")
+        yield attributes
+    
+    def parse_printed(self,response):       
+        print("Parsing........")
+        attributes =  response.meta["attributes"]
+        yield attributes
---
+++ scrapy_spider/spiders/MySpiderForPlayers.py
@@ -0,0 +1,151 @@
+import scrapy
+from scrapy.spiders import CrawlSpider,  Rule
+from scrapy.linkextractors import LinkExtractor
+import re
+
+class MySpiderForPlayers(CrawlSpider):
+    user_agent ="Custom"
+    name = "player_stats"
+    allowed_domains = ["www.transfermarkt.com"]
+    #start_urls = ["https://www.transfermarkt.com/wettbewerbe/europa/wettbewerbe?ajax=yw1&page=1"]
+    start_urls =  ['https://www.transfermarkt.com/premier-league/startseite/wettbewerb/GB1']
+    custom_settings = {
+        "DOWNLOAD_DELAY" : 4,
+        "DOWNLOAD_TIMEOUT" : 900000,
+        "CONCURRENT_REQUESTS" : 20,
+        'FEEDS': {
+            '%(name)s.csv': {
+                'format': 'csv',
+                'overwrite': True
+            }
+        }
+    }    
+    
+    rules = (
+    #    Rule(LinkExtractor(allow = r'profil\/spieler'), callback='parse_players', follow=True)
+        Rule(LinkExtractor(allow = r'startseite\/verein.*saison_id\/\d{3}2'), callback='parse_club_links', follow=False),
+     #   Rule(LinkExtractor(allow = r'profil\/spieler'), callback='parse_players', follow=True)
+    )
+    
+
+    def parse_club_links(self, response):
+        club_links = response.xpath('//*[@class="responsive-table"]//tbody//a//@href')
+        for title in club_links.extract() :
+            if "/profil/spieler/" in title:            
+                yield scrapy.Request(response.urljoin(title), callback=self.parse_players, headers={'User-Agent': 'Custom'})
+            else:
+                continue
+    
+    def parse_players(self, response):
+        print(response.url)
+        attributes = {}
+    
+        try:   
+            attributes['current_shirt_number'] = response.xpath('//span[@class = "data-header__shirt-number"]/text()'\
+                                                        ).extract_first().strip()
+        except:
+            attributes['current_shirt_number'] = ""
+        try : 
+            attributes['firstname'] = response.xpath('//span[@class = "data-header__shirt-number"]/../text()'\
+                                               ).extract()[1].strip()
+        except : 
+            attributes['firstname'] = ""
+        attributes['lastname'] = response.xpath('//span[@class = "data-header__shirt-number"]/following-sibling::*/text()'\
+                                        ).extract_first().strip()
+        
+        try :
+            attributes['int_goals'] = response.xpath('//a[@class = "data-header__content data-header__content--highlight"]/text()'\
+            ).extract()[1].strip()
+        except:
+            attributes['int_goals'] = 0
+
+        try:
+            attributes['int_caps'] = response.xpath('//a[@class = "data-header__content data-header__content--highlight"]/text()'\
+            ).extract()[0].strip()   
+        except:
+            attributes['int_caps'] = 0          
+#         attributes['main_position'] = response.xpath('//dd[@class = "detail-position__position"]/text()'\
+#           ).extract()[0].strip()   
+        attributes['main_position'] = response.xpath('//li[@class = "data-header__label" and contains(text(),"Position")]/span/text()'\
+          ).extract_first().strip()      
+        attributes['other_position'] = [x.strip() for x in response.xpath('//div[@class = "detail-position__title" and contains(text(),"Other position")]/following-sibling::*/text()').extract()]
+        try:
+            attributes['current_market_value'] = response.xpath('//div[@class = "tm-player-market-value-development__current-value"]/text()'\
+              ).extract_first().strip()    
+        except:
+            attributes['current_market_value'] = "unknown" 
+        try:
+            attributes['highest_market_value'] = response.xpath('//div[@class = "tm-player-market-value-development__max-value"]/text()'\
+              ).extract_first().strip()   
+        except:
+            attributes['highest_market_value'] = "unknown" 
+        try:
+            attributes['highest_market_value_on'] = response.xpath('//div[@class = "tm-player-market-value-development__max-value"]/following-sibling::*/text()'\
+              ).extract_first().strip()   
+        except:
+            attributes['highest_market_value_on'] = "unknown" 
+       # attributes['full_name'] = response.xpath('//span[contains(text(),"ame")]/following-sibling::*/text()').get().strip()
+        attributes['date_of_birth'] = response.xpath('//span[contains(text(),"Date of birth")]/following-sibling::*/a/text()').get().strip()
+        attributes['place_of_birth'] = response.xpath('//span[contains(text(),"Place of birth")]/following-sibling::*/span/text()').get().strip()
+        attributes['country_of_birth'] =  response.xpath('//span[contains(text(),"Place of birth")]/following-sibling::*/span/img/@alt').get().strip()
+        attributes['height'] = response.xpath('//span[contains(text(),"Height")]/following-sibling::*/text()').get().strip().replace("\xa0m","").replace(",","")            
+        attributes['citizenship'] = response.xpath('//span[contains(text(),"Citizenship")]/following-sibling::span/img[@class="flaggenrahmen"]/@alt').extract()
+        attributes['dominant_foot'] = response.xpath('//span[contains(text(),"Foot")]/following-sibling::*/text()').get().strip()            
+        attributes['current_club'] =  response.xpath('//span[contains(text(),"Current club")]/following-sibling::*/a/img/@alt').get().strip()           
+        attributes['joined_on'] =  response.xpath('//span[contains(text(),"Joined")]/span/text()').get().strip()             
+        attributes['contract_expiring'] =  response.xpath('//span[contains(text(),"Contract expires")]/following-sibling::*/text()').get().strip()              
+        try:
+            attributes['last_extension'] =  response.xpath('//span[contains(text(),"Date of last contract extension:")]/following-sibling::*/text()').get().strip()              
+        except:
+            attributes['last_extension'] =  "unknown"  
+        dictionary = {}
+        transfer_hist_list = []
+        dictionary["season"] = [x.strip() for x in response.xpath('//div[@class = "tm-player-transfer-history-grid"]/div[@class = "tm-player-transfer-history-grid__season"]/text()').extract()]
+        dictionary["transfer_date"] = [x.strip() for x in response.xpath('//div[@class = "tm-player-transfer-history-grid"]/div[@class = "tm-player-transfer-history-grid__date"]/text()').extract()]
+        dictionary["leaving_club"] = [x.strip() for x in response.xpath('//div[@class = "tm-player-transfer-history-grid"]/div[@class = "tm-player-transfer-history-grid__old-club"]/a/img/@alt').extract()]
+        dictionary["joining_club"] = [x.strip() for x in response.xpath('//div[@class = "tm-player-transfer-history-grid"]/div[@class = "tm-player-transfer-history-grid__new-club"]/a/img/@alt').extract()]
+        dictionary["market_value"] = [x.strip() for x in response.xpath('//div[@class = "tm-player-transfer-history-grid"]/div[@class = "tm-player-transfer-history-grid__market-value"]/text()').extract()]
+        dictionary["fee"] = [x.strip() for x in response.xpath('//div[@class = "tm-player-transfer-history-grid"]/div[@class = "tm-player-transfer-history-grid__fee"]/text()').extract()]
+        transfer_hist_list.append(dictionary)
+        attributes['transfer_history'] = dictionary
+        
+        text = response.xpath('//script[contains(text(),"skyscraper")]').extract_first()
+        text= text.replace("\n","")
+        text= text.replace("\'","")
+        text= text.replace("x20"," ")
+        text= text.replace("\\","")
+        text= text.replace("u20AC","Є")
+        result = re.search('data:\[\{(.*)\}\]\}\],legend', text)
+        data =result.group(1)
+        data = data.split(",")
+        dictionary2 = {}
+        dictionary2["club"] = []
+        dictionary2["market_value"] = []
+        dictionary2["age"] = []
+        dictionary2["date"] = []
+        dictionary2["year"] = []
+        for row in data:
+            try:
+                dictionary2["club"].append(re.match('verein:(.*).*', row).group(1))
+            except:
+                pass
+            try:
+                dictionary2["age"].append(re.match('age:(.*).*', row).group(1))
+            except:
+                pass
+            try:        
+                dictionary2["date"].append(re.match('datum_mw:(.*).*', row).group(1))
+            except:
+                pass
+            try:        
+                dictionary2["market_value"].append(re.match('mw:(.*).*', row).group(1))
+            except:
+                pass
+            try:        
+                dictionary2["year"].append(re.match(' (\d{4}).*', row).group(1))
+            except:
+                pass        
+        
+        attributes['valuation'] = dictionary2  
+        
+        yield attributes
